6530de0a6ed4d6180d79046b0c9fea6a21fde6b3
dilippoudel <dilippoudel578@gmail.com>
2024-08-22 13:21:15 +0300

    Add connection url to frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
+const apiUrl = '/choreo-apis/djangreact/backend/v1'
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(
